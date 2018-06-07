c9a3379cd6db1a2769974f12bbb457a59a016ae1
Subhajit Sahu <wolfram77@gmail.com>
2018-06-07 14:09:34 +0530

    :snowman: use github

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
 # pg-slang
-Convert informal SQL SELECT to formal SQL.
+
+[![NPM](https://nodei.co/npm/pg-slang.png)](https://nodei.co/npm/pg-slang/)
+
+Convert informal [SQL SELECT] to formal SQL.
+
+```sql
+-- ex: SLANG
+SELECT "food name", "calcium" FROM "apples"
+-- ex: SQL
+SELECT "name", "ca", "ca_e" FROM "compositions"
+WHERE TRUE AND (FALSE OR ("tsvector" @@ 'apples'))
+```
+
+```javascript
+const slang = require('pg-slang');
+// slang(<informal sql>, <map function>, [this], [options])
+// -> Promise <formal sql>
+
+// <informal sql>:
+// SELECT "food name", "trans fat" FROM "food" ORDER BY "trans fat" DESC
+// SELECT "protein", "vitamin d" FROM "poultry ORDER BY "vitamin d" DESC
+// SELECT "sum: essential amino acids" AS "amino" FROM "meat" ORDER BY "amino"
+// ...
+
+// <map function>(<text>, <type>, <hint>):
+// - text: field name, like "food name", "trans fat", "food", ...
+// - type: field type, can be "column" or "table"
+// - hint: field hint, can be null, "all", "sum", or "avg"
+// -> Promise [<value>]
+// - value: expression string
+
+// [options]:
+// - from: default table
+// - limit: default maximum limit
+// - limits: table specific maximum limts
+```
+
+```javascript
+// 1. 
+function fnA(text, type, hint) {
+  return ['sample1', 'sample1'];
+};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnA).then(console.log);
+// SELECT "sample", "sample" FROM "sample" WHERE TRUE AND TRUE
+
+
+function fnB(text, type, hint) {
+  if(type==='column') return ['name'];
+  return ['compositions'];
+};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnB).then(console.log);
+// SELECT "name", "name" FROM "compositions" WHERE TRUE AND TRUE
+
+
+function fnC(text, type, hint) {
+  if(type==='column') return ['ca', 'ca_e'];
+  return ['compositions'];
+};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnC).then(console.log);
+// SELECT "ca", "ca_e", "ca", "ca_e" FROM "compositions" WHERE TRUE AND TRUE
+
+
+var columns = {
+  'food code': ['code'],
+  'food name': ['name'],
+  'calcium': ['ca', 'ca_e'],
+  'magnesium': ['mg', 'mg_e']
+};
+var tables = ['food', 'compositions'];
+function fnD(text, type, hint) {
+  if(type==='column') return columns[text];
+  return tables.includes(text)? ['compositions']:[];
+};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnD).then(console.log);
+// SELECT "name", "ca", "ca_e" FROM "null" WHERE TRUE AND TRUE
+
+
+function fnE(text, type, hint) {
+  if(type==='column') return columns[text];
+  return tables.includes(text)? ['compositions']:[`"tsvector" @@ '${text}'`];
+};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnE).then(console.log);
+// SELECT "name", "ca", "ca_e" FROM "null" WHERE TRUE AND (FALSE OR ("tsvector" @@ 'apples'))
+
+
+var options = {from: 'compositions'};
+slang(`SELECT "food name", "calcium" FROM "apples"`, fnE, null, options).then(console.log);
+// SELECT "name", "ca", "ca_e" FROM "compositions" WHERE TRUE AND (FALSE OR ("tsvector" @@ 'apples'))
+
+
+// PRIMARY USECASE
+// ---------------
+function fnDB(text, type, hint) {
+  return new Promise((resolve, reject) => {
+    // ...
+    // <do some database lookup>
+    // ...
+  });
+};
+slang(/*  */, fnDB, null, options).then(console.log);
+// SELECT "name", "ca", "ca_e" FROM "compositions" WHERE TRUE AND (FALSE OR ("tsvector" @@ 'apples'))
+
+
+// NOTES
+// -----
+// 1. Map function return value can be an expression array
+// 2. For column, return multiple values to select multiple columns
+// 3. But in expressions, only first return value is considered
+// 4. Hints perform an operation on matching columns
+// 5. Use hint to decide which columns to return
+// 6. For table, returning expression will append to where
+// 7. Return expression and table name for full association
+// 8. Hint can be used in column text as "<hint>: <column text>"
+// 9. Hint "all": all|each|every
+// 10. Hint "sum": sum|gross|total|whole|aggregate
+// 11. Hint "avg": avg|mid|par|mean|norm|center|centre|average|midpoint
+// 12. Experiment! And mail me for issues / pull requests!
+```
+
+
+[SQL SELECT]: https://www.postgresql.org/docs/10/static/sql-select.html
---
+++ index.js
@@ -0,0 +1,200 @@
+const Parser = require('flora-sql-parser').Parser;
+const astToSQL = require('flora-sql-parser').util.astToSQL;
+
+const NULL = /^null$/i;
+const BOOL = /^(true|false)$/i;
+const NUMBER = /^[\d\.\-e]$/i;
+const STRING = /^\'[^\']\'$/;
+const IDENTIFIER = /^\w+$|^\"[^\"]*\"$/;
+const HINT_ALL = /^(all|each|every):/i;
+const HINT_SUM = /^(sum|gross|total|whole|aggregate):/i;
+const HINT_AVG = /^(avg|mid|par|mean|norm|center|centre|average|midpoint):/i;
+const FROMT = [{table: 't', as: null}];
+
+function number(value) {
+  return {type: 'number', value};
+};
+function column(column) {
+  return {type: 'column_ref', table: null, column};
+};
+function table(table, as=null) {
+  return {db: null, table, as};
+};
+function binaryExpr(operator, left, right, parentheses=false) {
+  return {type: 'binary_expr', operator, left, right, parentheses};
+};
+
+function dequote(txt) {
+  return /^[\'\"]/.test(txt)? txt.slice(1, -1):txt;
+};
+
+function clean(txt) {
+  txt = txt.replace(/\/\*.*?\*\//gm, '');
+  txt = txt.replace(/\-\-.*/g, '').trim();
+  return txt.endsWith(';')? txt.slice(0, -1):txt;
+};
+
+function setLimit(ast, max) {
+  var value = Math.min(ast.limit? ast.limit[1].value:max, max);
+  ast.limit = [{type: 'number', value}];
+};
+
+function getSum(cols) {
+  if(cols.length===0) return [number(0)];
+  if(cols.length===1) return [cols[0]];
+  var asw = binaryExpr('+', cols[0], cols[1], true);
+  for(var i=2, I=cols.length; i<I; i++)
+    asw.right = binaryExpr('+', asw.right, cols[i]);
+  return [asw];
+};
+
+function getAvg(cols) {
+  if(cols.length===0) return [number(0)];
+  var asw = binaryExpr('/', getSum(cols)[0], number(cols.length), true);
+  return [asw];
+};
+
+function parseExpression(exp) {
+  exp = exp.replace(/<>/g, '!=').replace(/@@/g, '<>');
+  var txt = `SELECT * FROM T WHERE (${exp})`;
+  var asw = new Parser().parse(txt);
+  return asw.where;
+};
+
+function parseValue(val) {
+  if(NULL.test(val)) return {type: 'null', value: null};
+  if(BOOL.test(val)) return {type: 'bool', value: /true/i.test(val)};
+  if(NUMBER.test(val)) return {type: 'number', value: parseFloat(val)};
+  if(STRING.test(val)) return {type: 'string', value: val.slice(1, -1)};
+  if(IDENTIFIER.test(val)) return column(dequote(val));
+  return parseExpression(val);
+};
+
+async function getColumn(txt, fn, ths=null) {
+  var type = 'column', hint = null;
+  if(HINT_ALL.test(txt)) hint = 'all';
+  else if(HINT_SUM.test(txt)) hint = 'sum';
+  else if(HINT_AVG.test(txt)) hint = 'avg';
+  var ans = await fn.call(ths, hint? txt.replace(/.*?:/, ''):txt, type, hint);
+  ans = (ans||[]).map(val => parseValue(val));
+  if(hint==null || hint==='all') return ans;
+  return hint==='sum'? getSum(ans):getAvg(ans);
+};
+
+function setSubexpression(ast, k, fn, ths=null) {
+  if(ast[k]==null || typeof ast[k]!=='object') return Promise.resolve();
+  if(ast[k].type==='column_ref') return getColumn(ast[k].column, fn, ths).then(ans => ast[k]=ans[0]);
+  return Promise.all(Object.keys(ast[k]).map(l => setSubexpression(ast[k], l, fn, ths)));
+};
+
+function getExpression(ast, fn, ths=null) {
+  if(ast.type==='column_ref') return getColumn(ast.column, fn, ths);
+  return Promise.all(Object.keys(ast).map(k => setSubexpression(ast, k, fn, ths))).then(() => [ast]);
+};
+
+function asExpression(expr) {
+  var sql = astToSQL({type: 'select', from: FROMT, columns: [{expr, as: null}]});
+  return sql.substring(7, sql.length-9).replace(/([\'\"])/g, '$1$1');
+};
+
+function asColumn(col, len, as) {
+  return txt = len>1 && as!=null? as+': '+col:as;
+};
+
+async function tweakColumns(ast, fn, ths=null) {
+  var columns = ast.columns, to = [];
+  var ans = await Promise.all(columns.map(col => getExpression(col.expr, fn, ths)));
+  for(var i=0, I=columns.length; i<I; i++) {
+    var col = columns[i], exps = ans[i];
+    for(var exp of exps) {
+      if(exp.type!=='column_ref') to.push({expr: exp, as: col.as==null? asExpression(exp):col.as});
+      else to.push({expr: exp, as: asColumn(exp.column, exps.length, col.as)});
+    }
+  }
+  ast.columns = to;
+};
+
+function tweakWhere(ast, fn, ths=null) {
+  setSubexpression(ast, 'where', fn, ths);
+};
+
+function tweakHaving(ast, fn, ths=null) {
+  setSubexpression(ast, 'having', fn, ths);
+};
+
+async function tweakOrderBy(ast, fn, ths=null) {
+  var orderby = ast.orderby, to = [];
+  var ans = await Promise.all(orderby.map(col => getExpression(col.expr, fn, ths)));
+  for(var i=0, I=orderby.length; i<I; i++) {
+    var col = orderby[i], exps = ans[i];
+    for(var exp of exps)
+      to.push({expr: exp, type: col.type});
+  }
+  ast.orderby = to;
+};
+
+async function tweakGroupBy(ast, fn, ths=null) {
+  var groupby = ast.groupby, to = [];
+  var ans = await Promise.all(groupby.map(exp => getExpression(exp, fn, ths)));
+  for(var val of ans)
+    to.push.apply(to, val);
+  ast.groupby = to
+};
+
+function forkWhere(ast) {
+  var txt = `SELECT * FROM T WHERE TRUE AND TRUE`;
+  var asw = new Parser().parse(txt);
+  if(ast.where) {
+    asw.where.left = ast.where;
+    ast.where = asw.where;
+    ast.where.left.parentheses = true;
+  }
+  else ast.where = asw.where;
+  return ast;
+};
+
+function appendWhere(ast, exp) {
+  exp = exp.replace(/<>/g, '!=').replace(/@@/g, '<>');
+  var txt = `SELECT * FROM T WHERE FALSE OR (${exp})`;
+  var asw = new Parser().parse(txt);
+  var opr = asw.where.right.operator.replace(/<>/, '@@');
+  asw.where.right.operator = opr;
+  if(ast.where.right.value===true) {
+    ast.where.right = asw.where;
+    ast.where.right.parentheses = true;
+  }
+  else {
+    asw.where.left = ast.where.right.right;
+    ast.where.right.right = asw.where;
+  }
+  return ast;
+};
+
+async function tweakFrom(ast, fn, ths=null) {
+  var ast = forkWhere(ast), from = ast.from, to = new Map();
+  var ans = await Promise.all(from.map(b => fn.call(ths, b.table, 'table', null)||[]));
+  for(var vals of ans) {
+    for(var val of vals) {
+      if(IDENTIFIER.test(val)) to.set(dequote(val), table(dequote(val)));
+      else appendWhere(ast, val);
+    }
+  }
+  ast.from = Array.from(to.values());
+};
+
+function slang(txt, fn, ths=null, opt={}) {
+  var ast = new Parser().parse(clean(txt)), rdy = [];
+  if(ast.type!=='select') return Promise.reject(new Error(`Only SELECT supported <<${txt}>>.`));
+  if(typeof ast.columns!=='string') rdy.push(tweakColumns(ast, fn, ths));
+  if(ast.where!=null) rdy.push(tweakWhere(ast, fn, ths));
+  if(ast.having!=null) rdy.push(tweakHaving(ast, fn, ths));
+  if(ast.orderby!=null) rdy.push(tweakOrderBy(ast, fn, ths));
+  if(ast.groupby!=null) rdy.push(tweakGroupBy(ast, fn, ths));
+  return Promise.all(rdy).then(() => tweakFrom(ast, fn, ths)).then(() => {
+    if(ast.from.length===0) ast.from.push(table(opt.from||'null'));
+    var lim = opt.limits? opt.limits[ast.from[0].table]||0:opt.limit||0;
+    if(lim) setLimit(ast, lim);
+    return astToSQL(ast);
+  });
+};
+module.exports = slang;
